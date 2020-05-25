46b324130a4b03faf99d88c382efa9920ec6aeca
Vedran Bilopavlović <vbilopav@gmail.com>
2020-05-25 12:52:03 +0200

    Add project files.

---
+++ PgRoutiner/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace PgRoutiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var currentDir = Directory.GetCurrentDirectory();
+            var configBuilder = new ConfigurationBuilder()
+                .AddJsonFile(Path.Join(currentDir, "settings.json"), optional: true,
+                    reloadOnChange: false)
+                .AddJsonFile(Path.Join(currentDir, "appsettings.json"), optional: true,
+                    reloadOnChange: false)
+                .AddJsonFile(Path.Join(currentDir, "appsettings.Development.json"), optional: true,
+                    reloadOnChange: false)
+                .AddCommandLine(args);
+
+            var config = configBuilder.Build();
+            Settings.Value = new Settings();
+            config.GetSection("pgr").Bind(Settings.Value);
+        }
+    }
+}
---
+++ PgRoutiner/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PgRoutiner
+{
+    public class Settings
+    {
+        public string Connection { get; set; }
+
+        public static Settings Value { get; set; }
+    }
+}
