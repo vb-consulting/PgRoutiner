c6ab407e4b7a0a0c06024aa71a4476706b5e2330
Vedran Bilopavlović <vbilopav@gmail.com>
2022-01-03 13:11:32 +0100

    .NET6 Upgrade

--- PgRoutiner/build.bat
+++ PgRoutiner/build.bat
@@ -7,12 +7,12 @@ dotnet clean --configuration SelfContained
 
 REM win10-x64 publish
 REM *****************
-dotnet publish --runtime win10-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true --output "_exe\win10-x64"
+dotnet publish --runtime win10-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true --self-contained --output "_exe\win10-x64"
 
 REM linux-x64 publish
 REM *****************
-dotnet publish --runtime linux-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --output "_exe\linux-x64"
+dotnet publish --runtime linux-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained --output "_exe\linux-x64"
 
 REM osx-x64 publish
 REM *****************
-dotnet publish --runtime osx-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --output "_exe\osx-x64"
+dotnet publish --runtime osx-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained --output "_exe\osx-x64"
--- PgRoutiner/build.sh
+++ PgRoutiner/build.sh
@@ -8,15 +8,15 @@ dotnet clean --configuration SelfContained
 
 echo win10-x64 publish
 echo *****************
-echo dotnet publish --runtime win10-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true --output "_exe\win10-x64"
-dotnet publish --runtime win10-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true --output "_exe\win10-x64"
+echo dotnet publish --runtime win10-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true --self-contained --output "_exe\win10-x64"
+dotnet publish --runtime win10-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true --self-contained --output "_exe\win10-x64"
 
 echo linux-x64 publish
 echo *****************
-echo dotnet publish -r linux-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --output "_exe\linux-x64"
-dotnet publish --runtime linux-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --output "_exe\linux-x64"
+echo dotnet publish -r linux-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained --output "_exe\linux-x64"
+dotnet publish --runtime linux-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained --output "_exe\linux-x64"
 
 echo osx-x64 publish
 echo *****************
-echo dotnet publish -r osx-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --output "_exe\osx-x64"
-dotnet publish --runtime osx-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --output "_exe\osx-x64"
+echo dotnet publish -r osx-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained --output "_exe\osx-x64"
+dotnet publish --runtime osx-x64 --configuration SelfContained /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained --output "_exe\osx-x64"
